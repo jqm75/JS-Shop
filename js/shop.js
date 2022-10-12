1fdae61fdb71efd7b35122c6b9c2d4cfd9109b2b
Quim <joaquimpujol@gmail.com>
2022-10-12 15:59:45 +0200

    Cambios

--- js/shop.js
+++ js/shop.js
@@ -1,75 +1,10 @@
-// If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-let products = [
-	{
-		id: 1,
-		name: "Cooking oil",
-		price: 10.5,
-		type: "grocery",
-		offer: {
-			number: 3,
-			percent: 20,
-		},
-	},
-	{
-		id: 2,
-		name: "Pasta",
-		price: 6.25,
-		type: "grocery",
-	},
-	{
-		id: 3,
-		name: "Instant cupcake mixture",
-		price: 5,
-		type: "grocery",
-		offer: {
-			number: 10,
-			percent: 30,
-		},
-	},
-	{
-		id: 4,
-		name: "All-in-one",
-		price: 260,
-		type: "beauty",
-	},
-	{
-		id: 5,
-		name: "Zero Make-up Kit",
-		price: 20.5,
-		type: "beauty",
-	},
-	{
-		id: 6,
-		name: "Lip Tints",
-		price: 12.75,
-		type: "beauty",
-	},
-	{
-		id: 7,
-		name: "Lawn Dress",
-		price: 15,
-		type: "clothes",
-	},
-	{
-		id: 8,
-		name: "Lawn-Chiffon Combo",
-		price: 19.99,
-		type: "clothes",
-	},
-	{
-		id: 9,
-		name: "Toddler Frock",
-		price: 9.99,
-		type: "clothes",
-	},
-];
 // Array with products (objects) added directly with push(). Products in this array are repeated.
 let cartList = [];
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
 let cart = [];
 let total = 0;
-
+let counter = 0;
 // Exercise 1
 function buy(id) {
 	// 1. Loop for to the array products to get the item to add to cart
@@ -157,10 +92,10 @@ function applyPromotionsCart(cart) {
 function printCart() {
 	// Fill the shopping cart modal manipulating the shopping cart dom
 
-	var htmlCartList = cart.length == 0
+	let htmlCartList = cart.length == 0
 	? "<tr><th scope='row'>Empty</th><td></td><td></td><td></td><td></td></tr>" 
 	: "";
-	var subtotal = 0;
+	let subtotal = 0;
 	cart.forEach(function(product){
 	   htmlCartList += "<tr>"
 	   htmlCartList += "<th scope='row'>" + product.name + "</th><td>$" + product.price + "</td><td>" + product.quantity + "</td><td>";
@@ -174,6 +109,8 @@ function printCart() {
 		   subtotal += (product.quantity * product.price);
 	   }
 	   htmlCartList += "<td><i rel='" + product.id + "' class='fas fa-trash remove-product'></i></td></tr>";
+	
+	 
 	})
 	document.getElementById('cart_list').innerHTML = htmlCartList;
 	document.getElementById('total_price').innerHTML = subtotal;
